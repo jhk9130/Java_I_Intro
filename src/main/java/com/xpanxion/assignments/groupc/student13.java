8fdecda75f1b8194184fca41da7edc48c9366889
jhk9130 <hyunk66@gmail.com>
2022-04-25 16:59:37 -0400

    Update1

--- src/main/java/com/xpanxion/assignments/groupc/student13.java
+++ src/main/java/com/xpanxion/assignments/groupc/student13.java
@@ -2,63 +2,85 @@ package com.xpanxion.assignments.groupc;
 
 import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class student13 {
     public static void main(String[] args) {
-        ex_1();
-        ex_2();
-        ex_3();
+        ex1();
+        ex2();
+        ex3();
+        ex4();
     }
 
     //Ex. 1
     private static void ex1() {
+        System.out.println("Ex. 1");
         addNumbers(new ArrayList<>());
     }
     private static Integer addNumbers(ArrayList<Integer> numList) {
         int[] ints = {1, 2, 3};
-        ArrayList<Integer> newList = new ArrayList<Integer>();
-
+        numList = new ArrayList<Integer>();
         int sum = 0;
         for (int i = 0;i < ints.length; i++) {
-            newList.add(ints[i]);
-            sum = sum + newList.get(i);
+            numList.add(ints[i]);
+            sum = sum + numList.get(i);
         }
-        System.out.println("List wrapped in Integer: " + newList);
+        System.out.println("List wrapped in Integer: " + numList);
         System.out.println("Sum of the array: " + sum);
-
         return 0;
     }
 
     //Ex. 2
     private static void ex2() {
+        System.out.println("Ex. 2");
         avgNumbers(new ArrayList<>());
     }
     private static Float avgNumbers(ArrayList<Integer> numList) {
         int[] ints = {1, 2, 3};
-        ArrayList<Integer> newList = new ArrayList<Integer>();
+        numList = new ArrayList<Integer>();
 
         int sum = 0;
         for (int i = 0;i < ints.length; i++) {
-            newList.add(ints[i]);
-            sum = sum + newList.get(i);
+            numList.add(ints[i]);
+            sum = sum + numList.get(i);
         }
-        var avgArray = (float) sum / newList.size();
+        var avgArray = (float) sum / numList.size();
         System.out.println("Average of array: " + avgArray);
         return avgArray;
     }
 
     // Ex. 3
     private static void ex3() {
+        System.out.println("Ex. 3");
         doubleArray(new ArrayList<>());
     }
     private static void doubleArray(ArrayList<Integer> numList) {
         int[] ints = {1, 2, 3};
-        ArrayList<Integer> newList = new ArrayList<Integer>();
-
+        numList = new ArrayList<Integer>();
         for (int i = 0;i < ints.length; i++) {
-            newList.add(ints[i]);
-            newList.set(ints[i], (2*newList.get(ints[i])));
+            numList.add(2*ints[i]);
+        }
+        System.out.println("Double array: " + numList);
+    }
+
+    // Ex. 4
+    private static void ex4() {
+        System.out.println("Ex. 4");
+        Scanner sc = new Scanner(System.in);
+        var maxNum = sc.nextInt();
+        System.out.println("Enter value to create list: " + maxNum);
+        createList(maxNum);
+    }
+    private static void createList(Integer maxNumber) {
+        ArrayList<Integer> newList = new ArrayList<Integer>();
+        for (int i = 0; i < maxNumber; i++) {
+            newList.add(i+1);
         }
         System.out.println(newList);
     }
+
+    private static void ex5() {
+
+    }
+
 }
